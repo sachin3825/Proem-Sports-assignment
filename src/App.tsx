db8cc1b2a5c0af0aaed4b199f32f9c59895901e8
Sachin kumawat <sachinkumawat55@gmail.com>
2025-06-17 17:10:54 +0530

    bug fix : light mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
 import Index from "@/pages/Index";
 import NotFound from "@/pages/NotFound";
-import { ThemeProvider } from "./context/theme-provider";
+
 // import { ModeToggle } from "./components/moon-toggle";
 const App = () => {
   return (
-    <ThemeProvider>
+    <>
       <Toaster />
       {/* <ModeToggle /> */}
       <BrowserRouter>
@@ -15,7 +15,7 @@ const App = () => {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-    </ThemeProvider>
+    </>
   );
 };
 
